c04b4ff96abc16e2fbcba80c23ac6db2ac11e672
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-02 12:43:15 +0530

    Add StyledTopBar component

---
+++ app/Components/StyledTopBar.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledTopBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 1em;
+  height: 3em;
+  background-color: #f6f8fa;
+  border-bottom: 1px solid #d1d5d8;
+`;
+
+export default StyledTopBar;
