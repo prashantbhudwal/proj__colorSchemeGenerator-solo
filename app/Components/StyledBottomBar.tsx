f3bdb7331f7617848819a52cff0d5de18fdfa050
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-02 12:46:27 +0530

    Add a StyledBottomBar component

---
+++ app/Components/StyledBottomBar.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledBottomBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 1em;
+  height: 3em;
+  background-color: #f6f8fa;
+  border-top: 1px solid #d1d5d8;
+`;
+
+export default StyledBottomBar;
