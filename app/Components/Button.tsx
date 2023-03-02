fb21950454f2166cf0107d3c77be5095a6e5975c
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-02 09:29:18 +0530

    Import and use Inter font

--- app/Components/Button.tsx
+++ app/Components/Button.tsx
@@ -1,11 +1,19 @@
 import styled from "styled-components";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const StyledButton = styled.button`
-  background: red;
-  color: white;
-  border: none;
-  padding: 10px;
+  background-color: white;
+  border: 1px solid #d1d5d8;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05);
+  padding: 0.2em 0.4em;
   font-size: 20px;
+  font-family: ${inter};
+  border-radius: 4px;
 `;
 
 export default function Button({ children }: any) {
