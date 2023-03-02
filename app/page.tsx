4efcce581b655afa1b15529b42ea7681fe33a5e7
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-02 09:32:30 +0530

    Add container for the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 "use client";
-
+import styled from "styled-components";
 import Button from "./Components/Button";
 
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
 export default function Home() {
-  return <Button>Test</Button>;
+  return (
+    <StyledApp>
+      <Button>Get Color Scheme</Button>
+    </StyledApp>
+  );
 }
